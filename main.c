59fdbb499324d5bd706c7776fc222969b9e5bef3
Nikhil Wankhede <tonywankhedee@users.noreply.github.com>
2026-01-31 20:35:04 +0530

    Initialize SDL2 Window & CMake build

---
+++ main.c
@@ -0,0 +1,11 @@
+//Importing all necessary libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+
+int main()
+{
+    SDL_Window *pwindow = SDL_CreateWindow("Imgbuf", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900, 600, 0);
+    SDL_Delay(3000);
+
+}
